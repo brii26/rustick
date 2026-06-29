347924ff95f523f0d84e170dd051bd58f564d8bb
Brian Ricardo Tamin <19623154@mahasiswa.itb.ac.id>
2026-06-29 23:34:11 +0700

    feat(tick): test generate tick 100ms

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+mod generator;
+use generator::generate_tick;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+	// set interval 1ms
+	let mut interval = tokio::time::interval(
+		std::time::Duration::from_millis(100)
+	);
+
+	// infinite loop generate_tick every 1 ms
+	loop {
+		interval.tick().await;
+		let tick = generate_tick();
+		println!("{:?}", tick);
+	}
 }
